f4f5a3bcfc1215fe45b21bd99aed38aae1dc6b9f
Lerwadeschats <lebourhisalex11@gmail.com>
2024-11-09 19:44:25 +0100

    It's commit time 5 (❁´◡`❁)

--- Source/SmashUE/Private/MatchGameMode.cpp
+++ Source/SmashUE/Private/MatchGameMode.cpp
@@ -5,6 +5,7 @@
 #include "Arena/ArenaPlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 #include "SmashCharacter.h"
+#include "Arena/ArenaSettings.h"
 
 
 void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultsActors) 
@@ -47,20 +48,21 @@ void AMatchGameMode::BeginPlay()
 TSubclassOf<ASmashCharacter> AMatchGameMode::GetSmashCharacterClassFromInputType(
 	EAutoReceiveInput::Type InputType) const 
 {
+	const UArenaSettings* ArenaSettings = GetDefault<UArenaSettings>();
 	switch (InputType) 
 	{
 	case EAutoReceiveInput::Player0:
-		return SmashCharacterClassP0;
+		return ArenaSettings->SmashCharacterClassP0;
 
 	case EAutoReceiveInput::Player1:
-		return SmashCharacterClassP1;
+		return ArenaSettings->SmashCharacterClassP1;
 
 	case EAutoReceiveInput::Player2:
-		return SmashCharacterClassP2;
-
+		return ArenaSettings->SmashCharacterClassP2;
+		
 	case EAutoReceiveInput::Player3:
-		return SmashCharacterClassP3;
-
+		return ArenaSettings->SmashCharacterClassP3;
+		
 	default:
 		return nullptr;
 	}
